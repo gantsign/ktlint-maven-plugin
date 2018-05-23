cb81f5f0660c7c0bb48e7dd758a5ca75edf59e97
John Freeman <john.freeman.opensource@gmail.com>
2018-05-23 16:34:41 +0100

    Fixed license headers to match project license (#17)
    Project is MIT but file headers were Apache 2.

--- src/test/kotlin/com/github/gantsign/maven/plugin/ktlint/CheckMojoTest.kt
+++ src/test/kotlin/com/github/gantsign/maven/plugin/ktlint/CheckMojoTest.kt
@@ -4,17 +4,23 @@
  * %%
  * Copyright (C) 2018 GantSign Ltd.
  * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  * #L%
  */
 package com.github.gantsign.maven.plugin.ktlint
@@ -76,8 +82,8 @@ class CheckMojoTest {
         verify(log).debug("Discovered reporter 'checkstyle'")
         verify(log).debug("Initializing 'maven' reporter with {verbose=false}")
         verify(log).debug("checking: src/main/kotlin/example/Example.kt")
-        verify(log).debug("Style error > src/main/kotlin/example/Example.kt:23:39: Unnecessary semicolon")
-        verify(log).error("src/main/kotlin/example/Example.kt:23:39: Unnecessary semicolon")
+        verify(log).debug("Style error > src/main/kotlin/example/Example.kt:29:39: Unnecessary semicolon")
+        verify(log).error("src/main/kotlin/example/Example.kt:29:39: Unnecessary semicolon")
         verify(log).warn("Source root doesn't exist: src/test/kotlin")
         verifyNoMoreInteractions(log)
     }
@@ -114,9 +120,9 @@ class CheckMojoTest {
         verify(log).debug("Discovered reporter 'checkstyle'")
         verify(log).debug("Initializing 'maven' reporter with {verbose=true, group_by_file=true}")
         verify(log).debug("checking: src/main/kotlin/example/Example.kt")
-        verify(log).debug("Style error > src/main/kotlin/example/Example.kt:23:39: Unnecessary semicolon")
+        verify(log).debug("Style error > src/main/kotlin/example/Example.kt:29:39: Unnecessary semicolon")
         verify(log).error("src/main/kotlin/example/Example.kt")
-        verify(log).error(" 23:39 Unnecessary semicolon (no-semi)")
+        verify(log).error(" 29:39 Unnecessary semicolon (no-semi)")
         verify(log).warn("Source root doesn't exist: src/test/kotlin")
         verifyNoMoreInteractions(log)
     }
@@ -148,8 +154,8 @@ class CheckMojoTest {
         verify(log).debug("Discovered reporter 'checkstyle'")
         verify(log).debug("Initializing 'maven' reporter with {verbose=false}")
         verify(log).debug("checking: src/main/kotlin/example/Example.kt")
-        verify(log).debug("Style error > src/main/kotlin/example/Example.kt:23:39: Unnecessary semicolon")
-        verify(log).error("src/main/kotlin/example/Example.kt:23:39: Unnecessary semicolon")
+        verify(log).debug("Style error > src/main/kotlin/example/Example.kt:29:39: Unnecessary semicolon")
+        verify(log).error("src/main/kotlin/example/Example.kt:29:39: Unnecessary semicolon")
         verify(log).warn("Source root doesn't exist: src/test/kotlin")
         verifyNoMoreInteractions(log)
     }
@@ -184,7 +190,7 @@ class CheckMojoTest {
                     "file": "src/main/kotlin/example/Example.kt",
                     "errors": [
                         {
-                            "line": 23,
+                            "line": 29,
                             "column": 39,
                             "message": "Unnecessary semicolon",
                             "rule": "no-semi"
