2b92e7ed6de1d9748d937f8829fcd41179571925
John Freman <john.freeman.opensource@gmail.com>
2018-05-24 08:55:21 +0000

    Changed Maven site to deploy only on release
    Documentation should reflect current release.

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,4 +4,8 @@ set -e
 
 ./mvnw install --batch-mode --show-version --settings .travis/settings.xml
 
-./mvnw site-deploy -P site-deploy  --batch-mode --show-version --settings .travis/settings.xml
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    if [ "$TRAVIS_TAG" != "" ]; then
+        ./mvnw site-deploy -P site-deploy --batch-mode --show-version --settings .travis/settings.xml
+    fi
+fi
