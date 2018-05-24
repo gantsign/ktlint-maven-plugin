998577586a6caac12d8574b21559972138229a9b
John Freeman <john.freeman.opensource@gmail.com>
2018-05-24 11:13:27 +0100

    Added Bintray config (#28)
    For deploying releases.

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,6 +6,8 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_TAG" != "" ]; then
+        ./mvnw deploy -P publish-artifacts --batch-mode --show-version --settings .travis/settings.xml
+
         ./mvnw site-deploy -P site-deploy --batch-mode --show-version --settings .travis/settings.xml
     fi
 fi
