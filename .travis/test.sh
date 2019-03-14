5ee064a18f69b9ae80a0998f85c197dc713d0289
John Freeman <john.freeman.opensource@gmail.com>
2019-03-14 15:58:10 +0000

    Reduced minimum Maven version to 3.3.9
    For users who are using older Maven versions.
    
    Enhancement: resolves #192

--- .travis/test.sh
+++ .travis/test.sh
@@ -2,4 +2,11 @@
 
 set -e
 
-./mvnw install --batch-mode --show-version
+curl --silent 'https://get.sdkman.io' | bash
+echo sdkman_auto_answer=true > ~/.sdkman/etc/config
+source ~/.sdkman/bin/sdkman-init.sh
+sdk install maven "$MAVEN_VERSION"
+
+export M2_HOME="$HOME/.sdkman/candidates/maven/$MAVEN_VERSION"
+
+"$M2_HOME/bin/mvn" install --batch-mode --show-version
